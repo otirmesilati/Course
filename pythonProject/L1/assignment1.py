be5e1979da1f439e2ac5b1f1ae8168e74ad5aeaa
Otyr <otirmesi@gmail.com>
2024-09-19 20:44:43 +0300

    First Lecture
    Third Part of Lecture 1

---
+++ pythonProject/L1/assignment1.py
@@ -0,0 +1,39 @@
+"""
+TODO:
+ 1. Create a variable name first with value 7
+ 2. Create a variable name second with value 44.3
+ 3. Print result of adding first to second
+ 4. Print result of multiplying first by second
+ 5. Print result of dividing second by first
+
+"""
+
+# Assignment 1, Otir Mesilati
+
+first = 7   # first = int(7) also works
+second = float(44.3)   # second = 44.3 also works
+print(first + second)   # this will be float result - implicit conversion to float
+print(first * second)   # machine precision
+print(second/first)
+
+# Part B:
+a = "aviel"
+b = 'aviel "buskila"'
+c = "aviel \"buskila\""
+
+# Part C:
+my_number = 5 + 5
+print("result is: " + str(my_number))
+
+# Part D
+x = 5
+y = 2.36
+print(x + int(y))   # first: explicit conversion to int, and then to sum the ints
+
+# Part E
+
+
+# "Testing"
+print("\n\n----------------this part is for testing :D --------------------")
+print(first)
+print(second)
---
+++ pythonProject/L1/t2.py
@@ -0,0 +1,12 @@
+your_name = input("Enter your name: ")
+print("are your aviel?" + str(your_name == "aviel"))
+your_age = int(input("Enter your age: "))
+print(your_age >= 18)
+your_height = int(input("Enter your height(cm): "))
+print(your_height >= 180)
+
+if your_name == "aviel":
+    print("Hello Aviel")
+else:
+    print("Hello not Aviel")
+
