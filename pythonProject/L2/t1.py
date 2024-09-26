1da1615eeb198879b1965bfa4362210f3f7b78c0
Otyr <otirmesi@gmail.com>
2024-09-26 18:23:52 +0300

    Adding Second lecture first targil

---
+++ pythonProject/L2/t1.py
@@ -0,0 +1,21 @@
+a = 4
+b = "aviel"
+is_aviel_married = True
+d = ["benny", "otir", "tal"]
+if d[2] == "tal" and is_aviel_married or d[1] != "otir":
+    print("tal and aviel is married")
+elif d[0] == "benny" and a < 10:
+    print("benny is here")
+else:
+    print("no one is married")
+
+if a == 4:
+    print("a is married")
+elif a > 20:
+    print("a is greater than 20")
+elif 40 < a < 60:
+    print("a is less than 40")
+if a != b:
+    print("a != b")
+else:
+    print("a == b")
