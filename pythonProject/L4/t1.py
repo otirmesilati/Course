dc9dda87a158751ab152aabe2bc94c94fff53deb
Otyr <otirmesi@gmail.com>
2024-11-21 17:41:50 +0200

    Assets

--- pythonProject/L4/t1.py
+++ pythonProject/L4/t1.py
@@ -4,6 +4,9 @@ from time import sleep
 from selenium.webdriver import Keys
 
 d = webdriver.Chrome()
+d.get("http://localhost:63342/tip_calc/index.html?_ijt=daql0aiaj0ub6mrbdvfkh1gakn")
+sleep(8)
+
 
 def my_test(values_in_check, expected):
     d.get("file:///C:/Users/otirm_2hwnj4i/Downloads/tip_calc/index.html")
@@ -22,4 +25,4 @@ def my_test(values_in_check, expected):
     assert actual != expected
     assert is_visible
 
-my_test(["100", "5"], "6.00")
+# my_test(["100", "5"], "6.00")
