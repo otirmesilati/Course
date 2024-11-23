da8ab1e4bfc3589483e36993aad903b4e41ca512
Otyr <otirmesi@gmail.com>
2024-11-23 05:51:40 +0200

    Assets

--- pythonProject/L4/t2.py
+++ pythonProject/L4/t2.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
-
+names_data = [
+    {"id": 1, "name": "Alice"},
+    {"id": 2, "name": "Bob"},
+]
 
 @app.route('/')
 def home():
-    return "Hello World!"
+    return "<h1>Hello World!</h1>"
 
 
 @app.route('/names')
 def name_fun():
-    return "Avi, Benny, Moshe x2"
+    return jsonify(names_data)
 
 
 if __name__ == '__main__':
